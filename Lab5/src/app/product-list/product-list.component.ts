2db4dfac639e9c6e5bed1408786976a00cd1d7fc
tsoyjulia0 <98633575+tsoyjulia0@users.noreply.github.com>
2023-03-01 19:47:04 +0600

    initial

---
+++ Lab5/src/app/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { productList } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = productList;
+
+}
+
